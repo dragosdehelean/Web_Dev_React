2dc40d18a45750d8386b3045b2f920429411df05
Dragos Dehelean Alin <dragos.dehelean@selenis.ro>
2018-12-02 02:29:28 +0200

    tuning

---
+++ scoreboard/app.js
@@ -0,0 +1,5 @@
+const name = "Clementine";
+const myClass = "hello";
+const titlu = <h1 className={myClass}>My {/*name is*/} {name}!</h1>;
+
+ReactDOM.render(titlu, document.getElementById("root"));
